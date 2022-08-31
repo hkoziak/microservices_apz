b90bbbb643f26a081728ac98220cfa24ca62ad64
Halyna Koziak <galiakozyak@gmail.com>
2022-08-31 13:13:24 +0300

    WIP

--- services/facade_service.py
+++ services/facade_service.py
@@ -2,6 +2,7 @@ from template import ServiceTemplate, FACADE_SERVICE_HOST, FACADE_SERVICE_PORT, 
     LOGGING_SERVICE_PORT, MESSAGE_SERVICE_HOST, MESSAGE_SERVICE_PORT, beautify_address
 from flask import request, make_response
 from threading import Thread
+from msg_tool.msg_storage import ServiceNotAvailable
 import requests
 import time
 import random
@@ -12,7 +13,7 @@ class FacadeService(ServiceTemplate):
     def __init__(self):
         super().__init__("FacadeService")
         self.logging_services, self.log_services_off = [], []
-        self.message_service = None
+        self.message_services, self.msg_services_removed = [], []
         self.turn_off = False
         self.uuid = 100
         self.updater = Thread(target=self.update_log_services)
@@ -20,9 +21,25 @@ class FacadeService(ServiceTemplate):
         self.updater.start()
         self.internal_error = 500
         self.success = 200
+        self.queue = DistributedQueue()
 
         @self.app.route("/", methods=['POST', 'GET'])
         def facade():
+            if request.method == 'GET':
+                part1 = self.get_from_msg_server()
+                part2 = self.get_from_log_server()
+                if part1.status_code != self.success or part2.status_code != 200:
+                    return make_response("Internal Server Error", self.internal_error)
+                text = part2.text + part1.text
+                return make_response(text, self.success)
+            elif request.method == 'POST':
+                message = request.json["message"]
+                part1 = self.post_on_log_server(message)
+                part2 = self.post_on_msg_server(message)
+                if part1.status_code != self.success or part2.status_code != self.success:
+                    return make_response("Internal Server Error", self.internal_error)
+                return part1
+
             if request.method == 'GET':
                 part1 = self.get_request(self.message_service, "Message")
                 try:
@@ -94,12 +111,16 @@ class FacadeService(ServiceTemplate):
     def add_logging_service(self, path):
         self.logging_services.append(path)
 
+    def add_msg_service(self, path):
+        self.message_services.append(path)
+
     def remove_log_service(self, path):
         self.logging_services.remove(path)
         self.log_services_off.append(path)
 
-    def add_messages_service(self, path):
-        self.message_service = path
+    def remove_msg_service(self, msg_path):
+        self.message_services.remove(msg_path)
+        self.msg_services_removed.append(msg_path)
 
     def get_request(self, service, server_type):
         try:
@@ -112,16 +133,81 @@ class FacadeService(ServiceTemplate):
             return make_response("Service Exception", self.internal_error)
         return response
 
+    def post_request(self, service, text):
+        i = 0
+        data = {"uuid": self.uuid + 1, "msg": text}
+        try:
+            response = requests.post(service, json=data)
+        except requests.exceptions.RequestException as err:
+            print("Logging service[", service, "]error:", err)
+            raise ValueError
+        while i < 10:
+            if response.status_code == self.success:
+                break
+            elif response.status_code == 409:
+                self.uuid += 10
+                i += 1
+            else:
+                return make_response(f"Internal Service Error {response.text}", self.internal_error)
+            data = {"uuid": self.uuid + 1, "msg": text}
+            try:
+                response = requests.post(service, json=data)
+            except requests.exceptions.RequestException as err:
+                print("Logging service[ ", service, " ] error:", err)
+                return make_response("Internal Service Error", self.internal_error)
+        self.uuid += 1
+        return make_response("Success", self.success)
+
+    def post_on_log_server(self, msg):
+        while True:
+            log_service = random.choice(self.logging_services)
+            response = self.post_request(log_service, msg)
+            if response.status_code == 200:
+                break
+            else:
+                self.remove_log_service(log_service)
+        return response
+
+    def post_on_msg_server(self, msg):
+        try:
+            self.queue.put_msg(msg)
+        except ServiceNotAvailable as err:
+            return make_response(str(err), self.internal_error)
+        return make_response("Success", self.success)
+
+    def get_from_log_server(self):
+        while True:
+            log_service = random.choice(self.logging_services)
+            ans = self.get_request(log_service, "Logging")
+            if ans.status_code == self.success:
+                break
+            else:
+                self.remove_log_service(log_service)
+        return ans
+
+    def get_from_msg_server(self):
+        while True:
+            msg_server = random.choice(self.message_services)
+            ans = self.get_request(msg_server, "Message")
+            if ans.status_code == self.success:
+                break
+            else:
+                self.remove_msg_service(msg_server)
+        return ans
+
 
 def main():
     service = FacadeService()
     if len(sys.argv) == 1:
         service.add_logging_service(beautify_address(LOGGING_SERVICE_HOST, LOGGING_SERVICE_PORT))
-    elif len(sys.argv) == 2:
+        service.add_msg_service(beautify_address(MESSAGE_SERVICE_HOST, MESSAGE_SERVICE_PORT))
+    elif len(sys.argv) == 3:
         log_services = sys.argv[1].split(";")
+        msg_services = sys.argv[2].split(";")
         for i in log_services:
             service.add_logging_service(i)
-    service.add_messages_service(beautify_address(MESSAGE_SERVICE_HOST, MESSAGE_SERVICE_PORT))
+        for j in msg_services:
+            service.add_msg_service(j)
     service.run(FACADE_SERVICE_HOST, FACADE_SERVICE_PORT)
 
 
--- services/message_service.py
+++ services/message_service.py
@@ -1,28 +1,58 @@
 from template import ServiceTemplate, FACADE_SERVICE_HOST, FACADE_SERVICE_PORT, MESSAGE_SERVICE_HOST, \
     MESSAGE_SERVICE_PORT, beautify_address
 from flask import request, make_response
+from threading import Thread
+from msg_tool.msg_storage import MessageStorage, DistributedQueue, ServiceNotAvailable
+import time
+import sys
 
 
-class MessageServer(ServiceTemplate):
-    def __init__(self):
-        super().__init__("MessageService")
+class MessageService(ServiceTemplate):
+    def __init__(self, n):
+        super().__init__("MessageService" + str(n))
         self.facade_service = None
+        self.storage = MessageStorage()
+        self.queue = DistributedQueue()
+        self.id_for_msq = 500
+        self.queue_msg = Thread(target=self.post_msg)
+        self.queue_msg.daemon = True
+        self.queue_msg.start()
+        self.success = 200
+        self.off = False
 
         @self.app.route("/", methods=['POST', 'GET'])
         def message():
             if request.method == 'POST':
                 pass
             elif request.method == 'GET':
-                return make_response("not implemented yet", 200)
+                answer = self.storage.get_all_msgs()
+                return make_response(answer, self.success)
 
     def add_facade_service(self, path):
         self.facade_service = path
 
+    def post_msg(self):
+        while not self.off:
+            time.sleep(10)
+            if not self.queue.is_empty():
+                try:
+                    msg = self.queue.get_msg()
+                    print(msg)
+                    self.storage.save_msg(self.id_for_msq, msg)
+                    self.id_for_msq += 1
+                except ServiceNotAvailable as err:
+                    print(err)
+
 
 def main():
-    server = MessageServer()
-    server.add_facade_service(beautify_address(FACADE_SERVICE_HOST, FACADE_SERVICE_PORT))
-    server.run(MESSAGE_SERVICE_HOST, MESSAGE_SERVICE_PORT)
+    if len(sys.argv) == 1:
+        service = MessageService(1)
+        port = MESSAGE_SERVICE_PORT
+    elif len(sys.argv) == 4:
+        service = MessageService(sys.argv[1], sys.argv[2])
+        port = sys.argv[3]
+    service.add_facade_service(beautify_address(FACADE_SERVICE_HOST, FACADE_SERVICE_PORT))
+    service.run(MESSAGE_SERVICE_HOST, MESSAGE_SERVICE_PORT)
 
 
 if __name__ == "__main__":
